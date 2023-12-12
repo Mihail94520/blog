d5ee27e0e61367e0b4e88f80c2bd6954f19b820b
Mihail94520 <mihail.alexandru462@gmail.com>
2023-12-12 18:14:47 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,17 +10,30 @@ def index():
 
     return render_template('index.html', categories=categories)
 
+def extraire_date_et_titre(nom_fichier):
+    # Extraction de la date et du titre
+    date_str, titre = nom_fichier[:8], nom_fichier[8:-5].replace('_', ' ')
+    # Conversion de la date en format lisible
+    date = datetime.strptime(date_str, '%Y%m%d').strftime('%d %B %Y')
+    return date, titre, nom_fichier
+
 @app.route('/categorie/<nom>')
 def categorie(nom):
     categories = os.listdir('articles')
-    articles = os.listdir('articles/' + nom)
+    articles = [ extraire_date_et_titre(f) for f in os.listdir('articles/' + nom) ]
 
     return render_template('articles.html', categorie=nom, categories=categories, articles=articles)
 
 @app.route('/article/<categorie>/<article>')
 def article(categorie, article):
+    categories = os.listdir('articles')
+    date_str, titre, nom_fichier = extraire_date_et_titre(article)
+    # Lecture du contenu du fichier
+    with open('articles/' + categorie + '/' + article, 'r', encoding='utf-8') as file:
+        contenu = file.read()
+    return render_template('article.html', categorie=categorie, categories=categories, contenu=contenu, titre=titre, date=date_str)
 
-    return send_file('articles/' + categorie + '/' + article)
+    #return send_file('articles/' + categorie + '/' + article)
 
 
 @app.route('/somme/<nombre1>/<nombre2>')
@@ -33,6 +47,5 @@ def produit(nombre1, nombre2):
 
 if __name__ == '__main__':
     print('http://127.0.0.1:5000/') # domain name server
-    # os.chdir(os.path.dirname(__file__))
     app.run(debug=True)
 
